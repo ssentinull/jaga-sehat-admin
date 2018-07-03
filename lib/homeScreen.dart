f5ea8349be52d66c11050b4bef9c3d4af0d5eb54
Ibnu Ahsani <ibnu.muhari@gmail.com>
2018-07-03 12:49:54 +0700

    Create Landscape Layout for Home Screen

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -35,9 +35,9 @@ class HomeScreen extends StatelessWidget {
 
     var statistikRoute = MaterialPageRoute(
         builder: (BuildContext context) => StatisticsScreen());
-        
-    var downloadRoute = MaterialPageRoute(
-        builder: (BuildContext context) => MyHomePage());
+
+    var downloadRoute =
+        MaterialPageRoute(builder: (BuildContext context) => MyHomePage());
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -48,61 +48,126 @@ class HomeScreen extends StatelessWidget {
         appBar: CustomAppBar2(
           logoutButton: logoutButtons,
         ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-            ),
-            child: ListView(
-              children: <Widget>[
-                CustomButton(
-                    'Statistik', Icons.assessment, 0xFFC54C82, statistikRoute),
-                CustomButton('Data Individu', Icons.assignment_ind, 0xFF512E67,
-                    dataRoute),
-                CustomButton('Unggah', Icons.cloud_download, 0xFF512E67,
-                    downloadRoute),
-              ],
-            ),
-          ),
+        body: OrientationBuilder(
+          builder: (BuildContext context, Orientation orientation) {
+            return Center(
+              child: orientation == Orientation.portrait
+              ? Container(
+                padding: EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CustomButton(true, 'Statistik', Icons.assessment, 0xFFC54C82,
+                        statistikRoute),
+                    CustomButton(true, 'Data Individu', Icons.assignment_ind,
+                        0xFF512E67, dataRoute),
+                    CustomButton(true, 'Unduh', Icons.cloud_download, 0xFFFF6699,
+                        downloadRoute),
+                  ],
+                ),
+              )
+              : Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CustomButton(false, 'Statistik', Icons.assessment, 0xFFC54C82,
+                        statistikRoute),
+                    CustomButton(false, 'Data Individu', Icons.assignment_ind,
+                        0xFF512E67, dataRoute),
+                    CustomButton(false, 'Unduh', Icons.cloud_download, 0xFFFF6699,
+                        downloadRoute),
+
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class CustomButton extends StatelessWidget {
-  int hexVal;
-  String text;
-  IconData icon;
-  MaterialPageRoute route;
+Widget customPlacement(bool p, String text, IconData icon) {
+  if (p == true) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 60.0,
+          color: Colors.white,
+        ),
+        Text(
+          '$text',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32.0,
+          ),
+        ),
+      ],
+    );
+  }
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Icon(
+        icon,
+        size: 60.0,
+        color: Colors.white,
+      ),
+      Padding(
+        padding: EdgeInsets.all(4.0),
+      ),
+      Text(
+        '$text',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 32.0,
+        ),
+      ),
+    ],
+  );
+}
 
-  CustomButton(this.text, this.icon, this.hexVal, this.route);
+Widget customContainer(bool p, Widget button) {
+  if (p == true) {
+    return Container(
+      margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: button,
+    );
+  }
+  return Container(
+    margin: EdgeInsets.all(5.0),
+    child: button,
+  );
+}
+
+class CustomButton extends StatelessWidget {
+  final bool p;
+  final int hexVal;
+  final String text;
+  final IconData icon;
+  final MaterialPageRoute route;
+
+  CustomButton(this.p, this.text, this.icon, this.hexVal, this.route);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
-      child: RaisedButton(
-        elevation: 2.0,
-        padding: EdgeInsets.all(40.0),
-        color: Color(hexVal),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Icon(
-              this.icon,
-              size: 60.0,
-              color: Colors.white,
-            ),
-            Text(
-              '$text',
-              style: TextStyle(color: Colors.white, fontSize: 32.0),
-            ),
-          ],
-        ),
+    return customContainer(
+      p,
+      RaisedButton(
+        elevation: 1.5,
+        padding: EdgeInsets.all(35.0),
+        color: Color(this.hexVal),
+        child: customPlacement(this.p, this.text, this.icon),
         onPressed: () {
-          Navigator.of(context).push(this.route);
+          Navigator.of(context).push(route);
         },
       ),
     );
